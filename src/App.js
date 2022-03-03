f349c8c7a6b4f89b086e5e0fe9d563ca4206e0a0
Smlcrp <smlcrp@gmail.com>
2022-03-03 11:13:42 -0500

    fixed browserrouter error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
